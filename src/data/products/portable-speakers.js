d59b36aaa5ebc3d845f2c1cc24eef3e5d246b346
Ivan Saltykov <hcys@list.ru>
2020-10-13 17:36:55 +0300

    Initial Commit

---
+++ src/data/products/portable-speakers.js
@@ -0,0 +1,22 @@
+export default [
+    {
+        title: 'ПОРТАТИВНЫЕ КОЛОНКИ DIGMA S-33 (ЧЕРНЫЙ)',
+        image: 'portativnye_kolonki_digma_s_33_chernyj.jpg',
+        price: 1490
+    },
+    {
+        title: 'ПОРТАТИВНЫЕ КОЛОНКИ SONY SRS-X88 (ЧЕРНЫЙ)',
+        image: 'portativnye_kolonki_sony_srs_x88_chernyj.jpg',
+        price: 2190
+    },
+    {
+        title: 'ПОРТАТИВНЫЕ КОЛОНКИ SVEN 247 (ЧЕРНЫЙ)',
+        image: 'portativnye_kolonki_sven_247_chernyj.jpg',
+        price: 600
+    },
+    {
+        title: 'ПОРТАТИВНЫЕ КОЛОНКИ DIGMA S-36 (ЧЕРНЫЙ)',
+        image: 'portativnye_kolonki_digma_s_36_chernyj.jpg',
+        price: 4000
+    },
+];
---
+++ src/data/products/tablets.js
@@ -0,0 +1,22 @@
+export default [
+    {
+        title: 'ПЛАНШЕТ HUAWEI MEDIAPAD T5 10 16GB LTE (ЧЕРНЫЙ)',
+        image: 'planshet_huawei_mediapad_t5_10_16gb_lte_chernyj.jpg',
+        price: 18490
+    },
+    {
+        title: 'ПЛАНШЕТ HUAWEI MEDIAPAD T3 8 16GB LTE (СЕРЫЙ)',
+        image: '8416513910_1575277305.jpg',
+        price: 10990
+    },
+    {
+        title: 'ПЛАНШЕТ OYSTERS T104WMI 10.1 3G 16G (ЧЕРНЫЙ)',
+        image: 'planshet_oysters_t104wmi_10_1_3g_16g_chernyj.jpg',
+        price: 10499
+    },
+    {
+        title: 'ПЛАНШЕТ DIGMA PLATINA 7.86 3G (ЧЕРНЫЙ)',
+        image: 'planshet_digma_platina_7_86_3g_chernyj.jpg',
+        price: 10399
+    },
+];
